687d77388dd6e02c6a1e628f43367939e413e5da
AndresChavez2021 <andresintel773@gmail.com>
2021-02-01 19:33:30 -0700

    no se

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::resource('oficios', OficioController::class)->parameters(['oficios' => 'tag'])->names('admin.oficios');
+
 Route::resource('clientes', ClienteController::class)->parameters(['clientes' => 'tag'])->names('admin.clientes');
+
 Route::resource('servicios', ServicioController::class)->parameters(['servicios' => 'tag'])->names('admin.servicios');
 Route::resource('secciones', SeccionController::class)->parameters(['secciones' => 'tag'])->names('admin.secciones');
 Route::resource('entrenamientos', EntrenamientoController::class)->parameters(['entrenamientos' => 'tag'])->names('admin.entrenamientos');
